aeb9df17742cdadd8d0acd2b7a8e36c3b07d4ba7
learningpocket <83200073+learningpocket@users.noreply.github.com>
2021-06-21 12:34:34 +0530

    Add files via upload

---
+++ CPP OPPs/CtoCpp.cpp
@@ -0,0 +1,15 @@
+// #include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x=10,y;
+
+    // printf()
+    cout << " Hi there !! " << x << endl << " Enter value of y : ";
+    cin >> y; // scanf()
+    cout << " Value of y is :" << y;
+
+    return 0;
+}
